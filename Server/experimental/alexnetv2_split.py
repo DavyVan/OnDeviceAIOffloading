36c3492012baa78a082f0c7ee8583b9d61fb2b6b
DavyVan <affqqa@163.com>
2017-10-23 16:53:23 +0800

    Added script for spliting the AlexNet V2

--- Server/experimental/alexnetv2_split.py
+++ Server/experimental/alexnetv2_split.py
@@ -1,150 +1,191 @@
-# We use this version 2 alexnet ONLY for convenience.
-# We do the surgary before the first fully connected layer.
-
-# ==============================================================================
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Contains a model definition for AlexNet.
-
-This work was first described in:
-  ImageNet Classification with Deep Convolutional Neural Networks
-  Alex Krizhevsky, Ilya Sutskever and Geoffrey E. Hinton
-
-and later refined in:
-  One weird trick for parallelizing convolutional neural networks
-  Alex Krizhevsky, 2014
-
-Here we provide the implementation proposed in "One weird trick" and not
-"ImageNet Classification", as per the paper, the LRN layers have been removed.
-
-Usage:
-  with slim.arg_scope(alexnet.alexnet_v2_arg_scope()):
-    outputs, end_points = alexnet.alexnet_v2(inputs)
-
-@@alexnet_v2
-"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+# Transparented from alexnetv2_all.py
 
 import tensorflow as tf
 import numpy as np
 
-slim = tf.contrib.slim
-trunc_normal = lambda stddev: tf.truncated_normal_initializer(0.0, stddev)
+import alexnetv2_all
+from tensorflow.python.tools import freeze_graph
+from tensorflow.python.framework import graph_io
 
-
-def alexnet_v2_arg_scope(weight_decay=0.0005):
-  with slim.arg_scope([slim.conv2d, slim.fully_connected],
-                      activation_fn=tf.nn.relu,
-                      biases_initializer=tf.constant_initializer(0.1),
-                      weights_regularizer=slim.l2_regularizer(weight_decay)):
-    with slim.arg_scope([slim.conv2d], padding='SAME'):
-      with slim.arg_scope([slim.max_pool2d], padding='VALID') as arg_sc:
-        return arg_sc
-
-
-def alexnet_v2(inputs,
-               num_classes=1000,
-               is_training=True,
-               dropout_keep_prob=0.5,
-               spatial_squeeze=True,
-               scope='alexnet_v2'):
-  """AlexNet version 2.
-
-  Described in: http://arxiv.org/pdf/1404.5997v2.pdf
-  Parameters from:
-  github.com/akrizhevsky/cuda-convnet2/blob/master/layers/
-  layers-imagenet-1gpu.cfg
-
-  Note: All the fully_connected layers have been transformed to conv2d layers.
-        To use in classification mode, resize input to 224x224. To use in fully
-        convolutional mode, set spatial_squeeze to false.
-        The LRN layers have been removed and change the initializers from
-        random_normal_initializer to xavier_initializer.
-
-  Args:
-    inputs: a tensor of size [batch_size, height, width, channels].
-    num_classes: number of predicted classes.
-    is_training: whether or not the model is being trained.
-    dropout_keep_prob: the probability that activations are kept in the dropout
-      layers during training.
-    spatial_squeeze: whether or not should squeeze the spatial dimensions of the
-      outputs. Useful to remove unnecessary dimensions for classification.
-    scope: Optional scope for the variables.
-
-  Returns:
-    the last op containing the log predictions and end_points dict.
-  """
-  with tf.variable_scope(scope, 'alexnet_v2', [inputs]) as sc:
-    end_points_collection = sc.name + '_end_points'
-    # Collect outputs for conv2d, fully_connected and max_pool2d.
-    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
-                        outputs_collections=[end_points_collection]):
-      net = slim.conv2d(inputs, 64, [11, 11], 4, padding='VALID',
-                        scope='conv1')
-      net = slim.max_pool2d(net, [3, 3], 2, scope='pool1')
-      net = slim.conv2d(net, 192, [5, 5], scope='conv2')
-      net = slim.max_pool2d(net, [3, 3], 2, scope='pool2')
-      net = slim.conv2d(net, 384, [3, 3], scope='conv3')
-      net = slim.conv2d(net, 384, [3, 3], scope='conv4')
-      net = slim.conv2d(net, 256, [3, 3], scope='conv5')
-      net = slim.max_pool2d(net, [3, 3], 2, scope='pool5')
-
-      # Use conv2d instead of fully_connected layers.
-      with slim.arg_scope([slim.conv2d],
-                          weights_initializer=trunc_normal(1.005),
-                          biases_initializer=tf.constant_initializer(0.1)):
-        net = slim.conv2d(net, 4096, [5, 5], padding='VALID',
-                          scope='fc6')
-        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                           scope='dropout6')
-        net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                           scope='dropout7')
-        net = slim.conv2d(net, num_classes, [1, 1],
-                          activation_fn=None,
-                          normalizer_fn=None,
-                          biases_initializer=tf.zeros_initializer(),
-                          scope='fc8')
-
-      # Convert end_points_collection into a end_point dict.
-      end_points = slim.utils.convert_collection_to_dict(end_points_collection)
-      if spatial_squeeze:
-        net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
-        end_points[sc.name + '/fc8'] = net
-      return net, end_points
-
+slim = alexnetv2_all.slim
 default_image_size = 224
 
-def main():
-  # Run AlexNet
-  with slim.arg_scope(alexnet_v2_arg_scope()):
-    # outputs, end_points = alexnet_v2(tf.Variable(tf.random_normal([1, 224, 224, 3]), dtype=tf.float32))
-    input_tensor = tf.placeholder(tf.float32, shape=[1, default_image_size, default_image_size, 3])
-    outputs, end_points = alexnet_v2(input_tensor)
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+# Take an input tensor with shape=[batch_size, height, width, channels],
+# which is [1, 224, 224, 3] in our case
+def alexnetv2_front(inputs, scope='alexnet_v2_front'):
+    with tf.variable_scope(scope, 'alexnet_v2_front', [inputs]) as sc:
+        end_points_collection = sc.name + '_end_points'
+        # Collect outputs for conv2d, fully_connected and max_pool2d.
+        with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d], outputs_collections=[end_points_collection]):
+            conv1 = slim.conv2d(inputs, 64, [11, 11], 4, padding='VALID', scope='conv1')
+            pool1 = slim.max_pool2d(conv1, [3, 3], 2, scope='pool1')
+            conv2 = slim.conv2d(pool1, 192, [5, 5], scope='conv2')
+            pool2 = slim.max_pool2d(conv2, [3, 3], 2, scope='pool2')
+            conv3 = slim.conv2d(pool2, 384, [3, 3], scope='conv3')
+            conv4 = slim.conv2d(conv3, 384, [3, 3], scope='conv4')
+            conv5 = slim.conv2d(conv4, 256, [3, 3], scope='conv5')
+            pool5 = slim.max_pool2d(conv5, [3, 3], 2, scope='pool5')
 
+            # Convert end_points_collection into a end_point dict.
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+            return pool5, end_points
+
+def run_front():
+    with slim.arg_scope(alexnetv2_all.alexnet_v2_arg_scope()):
+        input_tensor = tf.placeholder(tf.float32, shape=[1, default_image_size, default_image_size, 3])
+        outputs, end_points = alexnetv2_front(input_tensor)
+
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+
+        input_data = [float(i%255) for i in range(default_image_size * default_image_size * 3)]
+        input_data = np.array(input_data)
+        input_data = input_data.reshape((1, default_image_size, default_image_size, 3))
+
+        result = sess.run(outputs, {input_tensor: input_data})
+        print(outputs)
+        print(result[0][0][0])
+        writer = tf.summary.FileWriter('tfb_log', sess.graph)
+
+        freeze_front(sess)
+
+def freeze_front(sess):
+    # Freeze
+    front_saver = tf.train.Saver()
+    checkpoint_path = front_saver.save(sess, './save-front/front')
+    graph_io.write_graph(sess.graph, 'save-front', 'input_graph.pb')
+    input_graph_path = './save-front/input_graph.pb'
+    input_saver_def_path = ""
+    input_binary = False
+    output_node_names = "alexnet_v2_front/pool5/MaxPool"
+    restore_op_name = ""        # Deprecated
+    filename_tensor_name = ""   # Deprecated
+    output_graph_path = './save-front/output_graph.pb'
+    clear_devices = True
+    input_meta_graph = './save-front/front.meta'
+    freeze_graph.freeze_graph(input_graph_path,
+                                input_saver_def_path,
+                                input_binary,
+                                checkpoint_path,
+                                output_node_names,
+                                restore_op_name,
+                                filename_tensor_name,
+                                output_graph_path,
+                                clear_devices,
+                                "", "")
+
+def test_freeze_front():
+    # Creates graph from a pb file
+    with tf.gfile.FastGFile('./save-front/output_graph.pb', 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+    
+    sess = tf.Session()
+    input_tensor = sess.graph.get_tensor_by_name("Placeholder:0")
     input_data = [float(i%255) for i in range(default_image_size * default_image_size * 3)]
     input_data = np.array(input_data)
     input_data = input_data.reshape((1, default_image_size, default_image_size, 3))
 
-    sess.run(outputs, {input_tensor: input_data})
-    writer = tf.summary.FileWriter('tfb_log', sess.graph)
+    output_tensor = sess.graph.get_tensor_by_name('alexnet_v2_front/pool5/MaxPool:0')
+    
+    result = sess.run(output_tensor, {input_tensor: input_data})
+    print(result.shape)
+
+################################################################################
+
+# Take an input tensor with shape=[1, 5, 5, 256] in our case
+def alexnetv2_back(inputs, num_classes=1000, is_training=True, dropout_keep_prob=0.5, spatial_squeeze=True, scope='alexnet_v2_back'):
+    with tf.variable_scope(scope, 'alexnet_v2_back', [inputs]) as sc:
+        end_points_collection = sc.name + '_end_points'
+        # Collect outputs for conv2d, fully_connected and max_pool2d.
+        with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d], outputs_collections=[end_points_collection]):
+            # Use conv2d instead of fully_connected layers.
+            with slim.arg_scope([slim.conv2d], weights_initializer=alexnetv2_all.trunc_normal(1.005), biases_initializer=tf.constant_initializer(0.1)):
+                fc6 = slim.conv2d(inputs, 4096, [5, 5], padding='VALID',
+                                scope='fc6')
+                dropout6 = slim.dropout(fc6, dropout_keep_prob, is_training=is_training,
+                                scope='dropout6')
+                fc7 = slim.conv2d(dropout6, 4096, [1, 1], scope='fc7')
+                dropout7 = slim.dropout(fc7, dropout_keep_prob, is_training=is_training,
+                                scope='dropout7')
+                fc8 = slim.conv2d(dropout7, num_classes, [1, 1],
+                                activation_fn=None,
+                                normalizer_fn=None,
+                                biases_initializer=tf.zeros_initializer(),
+                                scope='fc8')
+                net = fc8
+            
+            # Convert end_points_collection into a end_point dict.
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+            if spatial_squeeze:
+                net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
+                end_points[sc.name + '/fc8'] = net
+            return net, end_points
+
+def run_back():
+    with slim.arg_scope(alexnetv2_all.alexnet_v2_arg_scope()):
+        input_tensor = tf.placeholder(tf.float32, shape=[1, 5, 5, 256])
+        outputs, end_points = alexnetv2_back(input_tensor)
+
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+
+        input_data = [float(i%20) for i in range(5 * 5 * 256)]
+        input_data = np.array(input_data)
+        input_data = input_data.reshape((1, 5, 5, 256))
+
+        result = sess.run(outputs, {input_tensor: input_data})
+        print(outputs)
+        writer = tf.summary.FileWriter('tfb_log', sess.graph)
+
+        freeze_back(sess)
+        
+def freeze_back(sess):
+    # Freeze
+    front_saver = tf.train.Saver()
+    checkpoint_path = front_saver.save(sess, './save-back/back')
+    graph_io.write_graph(sess.graph, 'save-back', 'input_graph.pb')
+    input_graph_path = './save-back/input_graph.pb'
+    input_saver_def_path = ""
+    input_binary = False
+    output_node_names = "alexnet_v2_back/fc8/squeezed"
+    restore_op_name = ""        # Deprecated
+    filename_tensor_name = ""   # Deprecated
+    output_graph_path = './save-back/output_graph.pb'
+    clear_devices = True
+    input_meta_graph = './save-back/back.meta'
+    freeze_graph.freeze_graph(input_graph_path,
+                                input_saver_def_path,
+                                input_binary,
+                                checkpoint_path,
+                                output_node_names,
+                                restore_op_name,
+                                filename_tensor_name,
+                                output_graph_path,
+                                clear_devices,
+                                "", "")
+
+def test_freeze_back():
+    # Creates graph from a pb file
+    with tf.gfile.FastGFile('./save-back/output_graph.pb', 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+    
+    sess = tf.Session()
+    input_tensor = sess.graph.get_tensor_by_name("Placeholder:0")
+    input_data = [float(i%255) for i in range(5 * 5 * 256)]
+    input_data = np.array(input_data)
+    input_data = input_data.reshape((1, 5, 5, 256))
+
+    output_tensor = sess.graph.get_tensor_by_name('alexnet_v2_back/fc8/squeezed:0')
+    
+    result = sess.run(output_tensor, {input_tensor: input_data})
+    print(result.shape)
 
 if __name__ == '__main__':
-  main()
+    # run_front()
+    # run_back()
+    # test_freeze_front()
+    test_freeze_back()
